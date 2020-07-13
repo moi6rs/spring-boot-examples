3b16a1fa4bfc06df3246365c0f0efd9b076700f2
mramoss <moi6ramoss@gmail.com>
2020-07-12 23:22:44 -0400

    create model person and change name data base

--- data-rest-example/src/main/java/com/ideasconnections/datarestexample/models/entity/Person.java
+++ data-rest-example/src/main/java/com/ideasconnections/datarestexample/models/entity/Person.java
@@ -1,4 +1,24 @@
 package com.ideasconnections.datarestexample.models.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "person")
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    @Getter
+    @Setter
+    @Column
+    private String name;
+
+    @Getter
+    @Setter
+    @Column
+    private String lastName;
 }
